2b2058bc7e04e905c28e4230e638a7982846147e
Herman Fung <herman_funghk@yahoo.com.hk>
2022-08-28 23:45:00 +0100

    add circle to map

--- src/components/module/map/map.jsx
+++ src/components/module/map/map.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import {
   GoogleMap,
   useLoadScript,
-  Marker /*, MarkerClusterer */,
+  Marker,
+  Circle /*, MarkerClusterer */,
 } from "@react-google-maps/api";
 // import { MarkerClusterer } from "@googlemaps/markerclusterer";
 import "../../../stylesheet/map.css";
@@ -101,6 +102,20 @@ const Map = () => {
             }}
           />
         ))}
+
+        <Circle
+          center={sampleLocations.homeLocations.find(
+            (location) => location.id === "HK"
+          )}
+          radius={800000}
+          options={{
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+          }}
+        />
       </GoogleMap>
     </React.Fragment>
   );
